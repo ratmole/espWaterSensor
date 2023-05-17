d74b170224e8aeb8381b2b7add53b537d9de0943
ratmole <ratmole@gmail.com>
2023-05-17 16:36:43 +0300

    /get page, min max and code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -43,25 +43,47 @@
   After the calibration the sensor is ready for measuring!
 **************************************************************/
 
-const char *ssid = "xxx";
-const char *password = "xxx";
-String serverName = "http://xxx/emoncms/input/post";
+const char *ssid = "ii";
+const char *password = "123qweasdzxc123qweasdzxc";
+String serverName = "http://ratmole.ddns.net/emoncms/input/post";
 
-String apiKey = "xxx";
+String apiKey = "388a981e8f2a02d4d644abde0dec5146";
 String inputName = "waterLevel";
 String inputNameSub1 = "waterPressure";
 String inputNameSub2 = "Voltage";
 String inputNameSub3 = "Percentage";
 
+String Name = "Water Level";
+
 int analogPin = A0;
 
-float maxR = 0;
-float minR = 1024;
+float minV, minP, minWP = 1024;
 
-unsigned long lastTime = 0;
+float V, P, WP, maxV, maxP, maxWP, lastTime = 0;
 unsigned long timerDelay = 2000;
-const float OffSet = 0.147;
-float V, P, WP;
+const float OffSet = 0.149;
+const float Max_ADCvoltage = 1.000;
+const float pressureMultiplier = 5.0;
+
+const char *html = "<html>"
+                   "<meta http-equiv='refresh' content='30'>"
+                   "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+                   "<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'><body><center>"
+                   "<p><b>Voltage: %PLACEHOLDER_VOLTAGE% V</b></p>"
+                   "<p>Voltage Min: %PLACEHOLDER_VMIN% V ,"
+                   "Voltage Max: %PLACEHOLDER_VMAX% V</p>"
+                   "<p><b>Pressure: %PLACEHOLDER_PRESSURE% kPa</b></p>"
+                   "<p>Pressure Min: %PLACEHOLDER_PMIN% kPa ,"
+                   "Pressure Max: %PLACEHOLDER_PMAX% kPa</p>"
+                   "<p><b>Percentage: %PLACEHOLDER_PERCENTAGE%</b></p>"
+                   "<p>Percentage Min: %PLACEHOLDER_WPMIN% ,"
+                   "Percentage Max: %PLACEHOLDER_WPMAX% </p></center>"
+                   "<div class='w3-container'>"
+                   "<div class='w3-border'>"
+                   "<div class='w3-green' style='height:24px;width:%PLACEHOLDER_PERCENTAGE%%'></div>"
+                   "</div>"
+                   "</div>"
+                   "</body></html>";
 
 void blink(int times)
 {
@@ -77,15 +99,31 @@ void blink(int times)
   }
 }
 
-void storeMinMax(float V)
+void storeMinMax(float V, float P, float WP)
 {
-  if (V < minR)
+  if (V < minV)
   {
-    minR = V;
+    minV = V;
   }
-  if (V > maxR)
+  if (V > maxV)
   {
-    maxR = V;
+    maxV = V;
+  }
+  if (P < minP)
+  {
+    minP = P;
+  }
+  if (P > maxP)
+  {
+    maxP = P;
+  }
+  if (WP < minWP)
+  {
+    minWP = WP;
+  }
+  if (WP > maxWP)
+  {
+    maxWP = WP;
   }
 }
 
@@ -108,6 +146,48 @@ float average(int analogPin, int samples, int delaymSec)
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+String processor(const String &var)
+{
+
+  if (var == "PLACEHOLDER_VOLTAGE")
+  {
+    return String(V);
+  }
+  else if (var == "PLACEHOLDER_PRESSURE")
+  {
+    return String(P);
+  }
+  else if (var == "PLACEHOLDER_PERCENTAGE")
+  {
+    return String(WP);
+  }
+  else if (var == "PLACEHOLDER_VMIN")
+  {
+    return String(minV);
+  }
+  else if (var == "PLACEHOLDER_VMAX")
+  {
+    return String(maxV);
+  }
+  else if (var == "PLACEHOLDER_PMIN")
+  {
+    return String(minP);
+  }
+  else if (var == "PLACEHOLDER_PMAX")
+  {
+    return String(maxP);
+  }
+  else if (var == "PLACEHOLDER_WPMIN")
+  {
+    return String(minWP);
+  }
+  else if (var == "PLACEHOLDER_WPMAX")
+  {
+    return String(maxWP);
+  }
+  return String();
 }
 
 AsyncWebServer server(80);
@@ -143,7 +223,10 @@ void setup()
   Serial.println(" seconds before publishing the first reading.");
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(200, "text/plain", "Hi!"); });
+            { request->send(200, "text/plain", Name); });
+
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", html, processor); });
 
   AsyncElegantOTA.begin(&server); // Start AsyncElegantOTA
   server.begin();
@@ -161,26 +244,40 @@ void loop()
       HTTPClient http;
 
       // Connect sensor to Analog ADC
-      V = average(analogPin, 1, 0) * 1.00 / 1024; // Sensor output voltage
-      storeMinMax(V);
-      P = (V - OffSet) * 250; // Calculate water pressure
-      WP = mapfloat((V - OffSet), 0.000, 1.005, 0.0, 100.0);
+      V = average(analogPin, 1, 0) * 1 / 1023; // Sensor output voltage
+
+      // P = (V - OffSet) * 250;             //Calculate water pressure for 5V ACD + 1 MPa pressure sensor
+      P = mapfloat((V - OffSet), 0.000, Max_ADCvoltage, 0.0, 10.0); // Calculate water pressure
+      WP = mapfloat((V - OffSet), 0.000, Max_ADCvoltage, 0.0, 100.0);
+      storeMinMax(V, P, WP);
 
       Serial.print("Voltage:");
       Serial.print(V, 3);
       Serial.print("V");
       Serial.print(", Pressure:");
-      Serial.print(P, 2);
-      Serial.print(" KPa");
+      Serial.print(P, 3);
+      Serial.print(" kPa");
       Serial.print(", Percentage:");
-      Serial.print(WP, 1);
+      Serial.print(WP, 3);
       Serial.print(" %");
       Serial.print(", V MIN:");
-      Serial.print(minR, 3);
+      Serial.print(minV, 3);
       Serial.print("V");
       Serial.print(" V MAX:");
-      Serial.print(maxR, 3);
+      Serial.print(maxV, 3);
       Serial.print("V");
+      Serial.print(", P MIN:");
+      Serial.print(minP, 3);
+      Serial.print("kPa");
+      Serial.print(" P MAX:");
+      Serial.print(maxP, 3);
+      Serial.print("kPa");
+      Serial.print(", WP MIN:");
+      Serial.print(minWP, 3);
+      Serial.print("%");
+      Serial.print(" WP MAX:");
+      Serial.print(maxWP, 3);
+      Serial.print("%");
       Serial.println();
 
       String serverPath = serverName + "/" + inputName + "?fulljson={%22" + inputNameSub1 + "%22:" + P + ",%22" + inputNameSub2 + "%22:" + V + ",%22" + inputNameSub3 + "%22:" + WP + "}&apikey=" + apiKey;
